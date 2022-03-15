db8200653c32f62faa6c9bd5fee4755a649fee70
gio_barranco <sandiego_94@hotmail.com>
2022-03-15 10:47:41 -0600

    updated src service worker file

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,6 +4,7 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { StaleWhileRevalidate } = require('workbox-strategies');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -27,4 +28,17 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  //callback function defined that filters requests we want to cache(JS and CSS files)
+  ({request}) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({
+    //name of cache storage
+    cacheName: "asset-cache",
+    plugins: [
+      //this plugin will cache responses with these headers to a max-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }),
+  );
